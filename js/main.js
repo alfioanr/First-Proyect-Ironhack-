b524f4bb7dc9fd0b81735f6a425d2c32a202a292
Alfio <alfioanr@gmail.com>
2018-01-25 14:13:46 +0100

    ya los pjs no se superponen

--- js/main.js
+++ js/main.js
@@ -10,47 +10,111 @@ window.onload = function() {
 };
 
 function update() {
-    if (game.players.positionX != game.players1.positionX && game.players.positionY != game.players1.positionY ){
-    game.players.drawPlayer();
-    game.players1.drawPlayer();
-}
-    // console.log("olis")
-    // console.log(game.players.positionX)
-     
+  // console.log("Jugardor1");
+  //console.log(game.players.positionX);
+  // console.log("Jugador2", game.players1.positionX, game.players1.positionY);
+
   ctx.clearRect(0, 0, 800, 800);
   game.board.drawBoard();
-    
+  game.players.drawPlayer();
+  game.players1.drawPlayer();
 }
+// console.log("olis")
+// console.log(game.players.positionX)
 
 setInterval(update, 50);
 
 document.onkeydown = function(e) {
   switch (e.keyCode) {
     case 38:
-      game.players.MovePlayer("up");
+      if (game.players.positionX == game.players1.positionX) {
+        if (game.players.positionY - 100 == game.players1.positionY) {
+          console.log("NO TE MUEVAS");
+        } else {
+          game.players.MovePlayer("up");
+        }
+      } else {
+        game.players.MovePlayer("up");
+      }
       break;
     case 40:
-      game.players.MovePlayer("down");
+      if (game.players.positionX == game.players1.positionX) {
+        if (game.players.positionY + 100 == game.players1.positionY) {
+          console.log("NO TE MUEVAS");
+        } else {
+          game.players.MovePlayer("down");
+        }
+      } else {
+        game.players.MovePlayer("down");
+      }
       break;
     case 37:
-      game.players.MovePlayer("left");
+      if (game.players.positionX - 100 == game.players1.positionX) {
+        if (game.players.positionY == game.players1.positionY) {
+          console.log("NO TE MUEVAS");
+        } else {
+          game.players.MovePlayer("left");
+        }
+      } else {
+        game.players.MovePlayer("left");
+      }
       break;
     case 39:
-      game.players.MovePlayer("right");
+      if (game.players.positionX + 100 == game.players1.positionX) {
+        if (game.players.positionY == game.players1.positionY) {
+          console.log("NO TE MUEVAS");
+        } else {
+          game.players.MovePlayer("right");
+        }
+      } else {
+        game.players.MovePlayer("right");
+      }
       break;
     case 87:
-      game.players1.MovePlayer("up");
+      if (game.players1.positionX == game.players.positionX) {
+        if (game.players1.positionY - 100 == game.players.positionY) {
+          console.log("NO TE MUEVAS");
+        } else {
+          game.players1.MovePlayer("up");
+        }
+      } else {
+        game.players1.MovePlayer("up");
+      }
+
       break;
     case 83:
-      game.players1.MovePlayer("down");
+      if (game.players1.positionX == game.players.positionX) {
+        if (game.players1.positionY + 100 == game.players.positionY) {
+          console.log("NO TE MUEVAS");
+        } else {
+          game.players1.MovePlayer("down");
+        }
+      } else {
+        game.players1.MovePlayer("down");
+      }
       break;
     case 65:
-      game.players1.MovePlayer("left");
+      if (game.players1.positionX - 100 == game.players.positionX) {
+        if (game.players1.positionY == game.players.positionY) {
+          console.log("NO TE MUEVAS");
+        } else {
+          game.players1.MovePlayer("left");
+        }
+      } else {
+        game.players1.MovePlayer("left");
+      }
       break;
     case 68:
-      game.players1.MovePlayer("right");
+      if (game.players1.positionX + 100 == game.players.positionX) {
+        if (game.players1.positionY == game.players.positionY) {
+          console.log("NO TE MUEVAS");
+        } else {
+          game.players1.MovePlayer("right");
+        }
+      } else {
+        game.players1.MovePlayer("right");
+      }
       break;
-     
-  }console.log (e.keyCode)
-   
+  }
+  console.log(e.keyCode);
 };
